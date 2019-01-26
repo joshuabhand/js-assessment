a44f4ed9e7ab3d999b934d59c31f5ef37b980cef
Joshua Hand <joshuabhand@gmail.com>
2019-01-26 14:48:31 -0600

    Completed flow control

--- app/flowControl.js
+++ app/flowControl.js
@@ -12,6 +12,23 @@ flowControlAnswers = {
    * @returns {String} The string 'fizz', 'buzz', or 'fizzbuzz'
    */
   fizzBuzz: function fizzBuzz(num) {
+        var result = '';
 
+        if(num === undefined || typeof num !== 'number') {
+            return false;
+        }
+
+        if(num % 3 === 0) {
+            result += 'fizz';
+        }
+        if(num % 5 === 0) {
+            result += 'buzz';
+        }
+
+        if(result === '') {
+            result = num;
+        }
+
+        return result;
   },
 };
